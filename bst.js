2ae8e2b876f97798c18292900a42cf353d68c461
Partha Protim Gogoi <176602154+g-partha@users.noreply.github.com>
2025-07-06 05:52:16 +0000

    Add find and levelOrder methods

--- bst.js
+++ bst.js
@@ -21,47 +21,74 @@ export class Tree {
   buildTree() {
     this.root = this.createBSTRec(this.array, 0, this.array.length - 1);
   }
-  addNewNodeRec(root, value){
-    if(root === null){
+  addNewNodeRec(root, value) {
+    if (root === null) {
       return new Node(value);
     }
-    if(root.data === value) return root;
-    if(value < root.data){
+    if (root.data === value) return root;
+    if (value < root.data) {
       root.left = this.addNewNodeRec(root.left, value);
     }
-    if(value > root.data){
+    if (value > root.data) {
       root.right = this.addNewNodeRec(root.right, value);
     }
     return root;
   }
-  insert(value){
+  insert(value) {
     this.root = this.addNewNodeRec(this.root, value);
   }
-  getSuccessor(root){
-    function smallestDataNode(rootNode){
-      if(rootNode.left === null) return rootNode;
+  getSuccessor(root) {
+    function smallestDataNode(rootNode) {
+      if (rootNode.left === null) return rootNode;
       return smallestDataNode(rootNode.left);
     }
-    if(root.right !== null){
+    if (root.right !== null) {
       return smallestDataNode(root.right);
     }
   }
-  removeNodeRec(root, value){
-    if(root === null) return root;
-    if(root.data > value){
+  removeNodeRec(root, value) {
+    if (root === null) return root;
+    if (root.data > value) {
       root.left = this.removeNodeRec(root.left, value);
-    } else if (root.data < value){
+    } else if (root.data < value) {
       root.right = this.removeNodeRec(root.right, value);
     } else {
-      if(root.left === null) return root.right;
-      if(root.right === null) return root.left;
+      if (root.left === null) return root.right;
+      if (root.right === null) return root.left;
       const successor = this.getSuccessor(root);
       root.data = successor.data;
       root.right = this.removeNodeRec(root.right, successor.data);
     }
     return root;
   }
-  deleteItem(value){
+  deleteItem(value) {
     this.root = this.removeNodeRec(this.root, value);
+  }
+  searchRec(root, value) {
+    if (root === null) return null;
+    if (root.data > value) {
+      return this.searchRec(root.left, value);
+    } else if (root.data < value) {
+      return this.searchRec(root.right, value);
+    } else {
+      return root;
+    }
+  }
+  find(value) {
+    return this.searchRec(this.root, value);
+  }
+  levelOrder(callback) {
+    if(callback === undefined){
+      throw new Error("callback is required!");
+    }
+    if (this.root === null) return null;
+    const queue = [this.root];
+    while (queue.length !== 0) {
+      const currentNode = queue.shift();
+      if (currentNode !== null) {
+        callback(currentNode);
+        queue.push(currentNode.left, currentNode.right);
+      }
+    }
   }
 }
